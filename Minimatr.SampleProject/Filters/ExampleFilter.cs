4648dbd276fb9c0d8696134bdd67d2e09fceecbc
Harish <tidak_ada_alamat@yahoo.co.id>
2022-08-12 14:02:26 +0700

    update sample

--- Minimatr.SampleProject/Filters/ExampleFilter.cs
+++ Minimatr.SampleProject/Filters/ExampleFilter.cs
@@ -1,9 +1,10 @@
 using Minimatr.RouteHandling.Filter;
 
-namespace Minimatr.SampleProject.Filters; 
+namespace Minimatr.SampleProject.Filters;
 
-public class ExampleFilter:IRouteHandlerFilter {
-    public ValueTask<object?> InvokeAsync(RouteHandlerInvocationContext context, RouteHandlerFilterDelegate next) {
-        throw new NotImplementedException();
+public class ExampleFilter : IRouteHandlerFilter {
+    public async ValueTask<object?> InvokeAsync(RouteHandlerInvocationContext context, RouteHandlerFilterDelegate next) {
+        Console.WriteLine("Filter here");
+        return await next(context);
     }
 }
