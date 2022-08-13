a8e8dd8df25e76dd16797557ecc1a58270ff0aa7
Harish <tidak_ada_alamat@yahoo.co.id>
2022-08-13 11:52:33 +0700

    add custom json converters

--- Minimatr.SampleProject/Program.cs
+++ Minimatr.SampleProject/Program.cs
@@ -1,17 +1,27 @@
 using System.Reflection;
 using MediatR;
+using Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.Routing.Patterns;
 using Microsoft.OpenApi.Models;
 using Minimatr;
 using Minimatr.Extensions;
 using Minimatr.OpenApi;
 using Minimatr.SampleProject;
+using Ngb.DateTime;
 
 // var pattern = RoutePatternFactory.Parse("/test/{id:int}");
 
 var builder = WebApplication.CreateBuilder(args);
 var assembly = Assembly.GetExecutingAssembly();
 var services = builder.Services;
+
+services.Configure<JsonOptions>(
+    options => {
+        options.SerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+        options.SerializerOptions.Converters.Add(new TimeOnlyJsonConverter());
+    }
+);
+
 services.AddMinimatr(assembly);
 services.AddMinimatrSchemaGenerator();
 services.AddMediatR(assembly);
