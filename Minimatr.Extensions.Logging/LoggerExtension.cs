f272a1cc5e1051fec4c2fdddd1a04acd30637762
Harish <tidak_ada_alamat@yahoo.co.id>
2022-09-13 22:43:58 +0700

    add logging extension

---
+++ Minimatr.Extensions.Logging/LoggerExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Minimatr.Extensions.Logging;
+
+public static class LoggerExtension {
+    public static bool IsTraceEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Trace);
+    public static bool IsDebugEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Debug);
+    public static bool IsInformationEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Information);
+    public static bool IsWarningEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Warning);
+    public static bool IsErrorEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Error);
+    public static bool IsCriticalEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Critical);
+}
