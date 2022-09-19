849914985ca555c4f5da2473240617fdbf23e6ca
Harish <tidak_ada_alamat@yahoo.co.id>
2022-09-19 21:35:08 +0700

    add documentations

--- Minimatr.Extensions.Logging/LoggerExtension.cs
+++ Minimatr.Extensions.Logging/LoggerExtension.cs
@@ -3,10 +3,34 @@
 namespace Minimatr.Extensions.Logging;
 
 public static class LoggerExtension {
+    /// <summary>
+    /// Checks if the trace level is enabled.
+    /// </summary>
+    /// <returns><c>true</c> if enabled.</returns>
     public static bool IsTraceEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Trace);
+    /// <summary>
+    /// Checks if the debug level is enabled.
+    /// </summary>
+    /// <returns><c>true</c> if enabled.</returns>
     public static bool IsDebugEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Debug);
+    /// <summary>
+    /// Checks if the information level is enabled.
+    /// </summary>
+    /// <returns><c>true</c> if enabled.</returns>
     public static bool IsInformationEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Information);
+    /// <summary>
+    /// Checks if the warning level is enabled.
+    /// </summary>
+    /// <returns><c>true</c> if enabled.</returns>
     public static bool IsWarningEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Warning);
+    /// <summary>
+    /// Checks if the error level is enabled.
+    /// </summary>
+    /// <returns><c>true</c> if enabled.</returns>
     public static bool IsErrorEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Error);
+    /// <summary>
+    /// Checks if the critical level is enabled.
+    /// </summary>
+    /// <returns><c>true</c> if enabled.</returns>
     public static bool IsCriticalEnabled(this ILogger logger) => logger.IsEnabled(LogLevel.Critical);
 }
